07c3002cc15e5412307c13d1d79d925d654f910e
Kittin Vatabutr <kittin.vat@hotmail.com>
2021-03-22 21:19:57 +0700

    [ch216] Refine code naming and add Thai locales

--- src/components/TextField/passwordField.tsx
+++ src/components/TextField/passwordField.tsx
@@ -2,48 +2,47 @@ import React, { useState } from 'react'
 import Image from 'next/image'
 
 interface PasswordField {
-  readonly label: String
+  readonly name: string
+  readonly label: string
+  readonly placeholder: string
   readonly errorInput: string
 }
 
-const PasswordField = ({ label, errorInput }: PasswordField) => {
+const PasswordField = ({ label, placeholder, errorInput }: PasswordField) => {
   const [isPasswordShown, setPasswordShown] = useState(false)
 
-  const togglePasswordVisiblity = () => {
+  const handlePasswordVisiblity = () => {
     setPasswordShown(!isPasswordShown)
   }
 
   const renderValidInput = () => (
-    <div className="form__input-group">
+    <>
       <input
-        placeholder="Enter your Password"
-        type={isPasswordShown ? 'text' : 'password'}
         className="form__input"
+        type={isPasswordShown ? 'text' : 'password'}
+        placeholder={placeholder}
       />
       {renderEyeIcon()}
-    </div>
+    </>
   )
 
   const renderInvalidInput = () => (
     <>
-      <div className="form__input-group">
-        <input
-          type={isPasswordShown ? 'text' : 'password'}
-          className="form__input form__input--invalid"
-        />
-        {renderEyeIcon()}
-      </div>
+      <input
+        className="form__input form__input--invalid"
+        type={isPasswordShown ? 'text' : 'password'}
+      />
+      {renderEyeIcon()}
       <div className="form__error">{errorInput}</div>
     </>
   )
 
   const renderEyeIcon = () => (
     <i className="form__eye-icon">
-      <Image
-        src={isPasswordShown ? '/icons/eye.svg' : '/icons/invisible.svg'}
+      <Image src={isPasswordShown ? '/icons/eye-icon.svg' : '/icons/invisible-eye-icon.svg'}
         height={17}
         width={21}
-        onClick={togglePasswordVisiblity}
+        onClick={handlePasswordVisiblity}
       />
     </i>
   )
@@ -52,7 +51,9 @@ const PasswordField = ({ label, errorInput }: PasswordField) => {
     <>
       <div className="form__group">
         <label className="form__input-label">{label}</label>
-        {!errorInput ? renderValidInput() : renderInvalidInput()}
+        <div className="form__input-password">
+          {!errorInput ? renderValidInput() : renderInvalidInput()}
+        </div>
       </div>
     </>
   )
